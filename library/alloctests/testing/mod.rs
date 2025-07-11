9eedd3aac275acf92c82f74e6e476f06c45811fd
Jay92-arch <jessehammelmann@hotmail.com>
2025-07-11 20:00:41 +1000

    Update mod.rs

--- library/alloctests/testing/mod.rs
+++ library/alloctests/testing/mod.rs
@@ -2,3 +2,7 @@ pub(crate) mod crash_test;
 pub(crate) mod macros;
 pub(crate) mod ord_chaos;
 pub(crate) mod rng;
+pub(crate) mod crash_test;
+pub(crate) mod macros;
+pub(crate) mod ord_chaos;
+pub(crate) mod rng;
